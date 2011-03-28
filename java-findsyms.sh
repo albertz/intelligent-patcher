8b99af671cd93ae2c85d73ef9cc1834f1aa7976e
Albert Zeyer <albert.zeyer@rwth-aachen.de>
2011-03-28 16:39:16 +0200

    read filelist from stdin

--- java-findsyms.sh
+++ java-findsyms.sh
@@ -1,12 +1,31 @@
 #!/bin/zsh
 
 mydir=$(dirname $0)
+if [ "$1" = "-v" ]; then
+	inverse=1
+	shift
+else
+	inverse=0
+fi
+string=$1
+dir=$2
 
-for f in $2/**/*.java; do
+check() {
+	f=$1
 	haveSym=0
 	$mydir/java-dumpsyms.py $f 2>/dev/null | { while read l; do
-		[ "$l" = "$1" ] && haveSym=1 && break
+		[ "$l" = "$string" ] && haveSym=1 && break
 	done; }
-	[ $haveSym = 1 ] && echo $f
-done
+	[ $inverse = 0 ] && [ $haveSym = 1 ] && echo $f
+	[ $inverse = 1 ] && [ $haveSym = 0 ] && echo $f
+}
 
+if [ "$dir" != "" ]; then
+	for f in $dir/**/*.java; do
+		check $f
+	done
+else
+	while read f; do
+		check $f
+	done
+fi
